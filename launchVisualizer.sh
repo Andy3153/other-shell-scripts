7741fb8d1b56f58c2fbd25086b24e00e92b7fcae
Andy3153 <andy3153@protonmail.com>
2022-10-25 08:37:01 +0300

    new script for cava in urxvt as visualizer

---
+++ launchVisualizer.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+## vim: set fenc=utf-8 ts=2 sw=0 sts=0 sr et si tw=0 fdm=marker fmr={{{,}}}:
+##
+## launchVisualizer.sh by Andy3153
+## created   18/10/22 ~ 11:47:12
+##
+## This script was made in order to start a visualizer on your screen to be used
+## as a background window.
+##
+## Inspired by https://github.com/GuidoFe/visualizer
+##
+## Dependencies:
+##    Required: rxvt-unicode cava
+##
+
+# {{{ Variables
+transparency='0'
+bg='black'
+font='Iosevka'
+fontsize='18'
+bordersize='0'
+# }}}
+
+urxvt -bg "[$transparency]$bg" -fn "xft:$font:pixelsize=$fontsize" -b $bordersize -depth 32 +sb -sl 0 -e cava
